c9fc2241da302da5b60c1e96beed2e79b8e91f84
Cam Gould <git@camgould.com>
2019-08-09 14:23:50 +1000

    Location callback

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -3,14 +3,22 @@
  * -----------------------------------------------------------------------------
  * Usage:
  *   import {getGeoLocation} from '@elkfox/shopify-theme/scripts/geolocation';
+ *
+ *   getGeo(function(location){
+ *     console.log(location);
+ *   });
  */
 
 export function getGeoLocation() {
-  const xhr = new window.XMLHttpRequest();
+  const xhr = new XMLHttpRequest;
 
   xhr.open('GET', '//geo.elkfox.net/json/');
-  xhr.onload = function() {
-    return JSON.parse(xhr.response);
-  };
   xhr.send();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var location = JSON.parse(xhr.response);
+
+      if(callback) callback(location);
+    };
+  }
 }
