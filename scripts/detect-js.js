1750283cfadbfbd6d38f1f03704b962b077aa1a9
Cam Gould <git@camgould.com>
2019-08-09 14:45:43 +1000

    JS detection alpha

---
+++ scripts/detect-js.js
@@ -0,0 +1,10 @@
+/**
+ * @elkfox/shopify-theme/scripts/detect-js
+ * -----------------------------------------------------------------------------
+ * Usage:
+ *   import {detectJs} from '@elkfox/shopify-theme/scripts/detect-js';
+ */
+
+export function detectJs() {
+  document.body.classList.add('js');
+}
