a99e2b0cfe641e91acde7602189aac1a1e8b41b2
Cam Gould <git@camgould.com>
2020-01-26 19:30:42 +1100

    Create product.js

---
+++ scripts/product.js
@@ -0,0 +1,35 @@
+// Fetch is not supported by IE so we use a polyfill for now
+import fetch from 'node-fetch';
+
+/**
+ * Get product
+ * ------------------------------------------------------------------------------
+ * Fetch any product as a JSON object
+ *
+ * Usage:
+ *   import {getProduct} from '@elkfox/shopify-theme/scripts/product';
+ *
+ *   getProduct();
+ *
+ *   // Then listen for the event, project-wide
+ *   document.addEventListener('product:get', (event) => {
+ *     console.log(event.detail.json);
+ *   });
+ */
+
+export function getProduct(productHandle) {
+  return fetch(`/products/${productHandle}.js`, {
+    method: 'GET',
+  }).then((response) => {
+    return response.json();
+  }).then((json) => {
+    const productEvent = new window.CustomEvent('product:get', {
+      detail: {
+        json,
+      },
+    });
+
+    document.dispatchEvent(productEvent);
+    return product;
+  });
+}
